84b4ff3ddd836198b4f0706cf7488055daa68a07
Alaabm <alaabasim@gmail.com>
2024-01-30 17:49:45 -0500

    completed lab

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import learnerData from './assets/data/learnerData'
+import Learner from './components/Learner'
+
+function App() {
+  
+  const [learners, setLearnerData] = useState(learnerData);
+
+  return (
+    <>
+      <Learner learners={learners} />
+    </>
+  )
+}
+
+export default App
+
